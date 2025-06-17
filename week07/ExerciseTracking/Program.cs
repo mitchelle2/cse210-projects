0c195eccf18ad43b9f3c74c1dbd22e59e5896147
mitchellealex <mitchellemadumere@gmail.com>
2025-06-17 21:31:36 +0100

    week 07 exercise projects

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine();
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("03 Nov 2022", 30, 3.0),
+            new Cycling("03 Nov 2022", 45, 15.0),
+            new Swimming("03 Nov 2022", 60, 20)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
